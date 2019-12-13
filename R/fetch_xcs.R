5dc80e3556f7503998f7ce32dee7b80473b34cfb
Chris Bailey <c.jamesbailey@gmail.com>
2019-12-13 13:18:12 +0000

    xcs can be output by match

--- R/fetch_xcs.R
+++ R/fetch_xcs.R
@@ -10,6 +10,7 @@
 #'
 #' @param year Season we want to download the xCS data for. For 2019/20 enter 2019.
 #' @param match_id Fetch xCS data for a single match, if you know the match ID. By default it is set to 'all'.
+#' @param ungroup Set this to TRUE if you want raw data for each game.
 #'
 #' @return a data.frame of the expected vs actual clean sheets, split by home/away.
 #'
@@ -17,7 +18,7 @@
 #' df <- fetch_player_data()
 #'
 
-fetch_xCS <- function(year = 2019, match_id = 'all', ...){
+fetch_xCS <- function(year = 2019, match_id = 'all', ungroup = F, ...){
   # get IDs of each game played
   understat_matches <- xml2::read_html(paste0('https://understat.com/league/EPL/',year))
 
@@ -39,7 +40,15 @@ fetch_xCS <- function(year = 2019, match_id = 'all', ...){
   # for each played game, scrape understat xCS
   understat_xCS <- lapply(match_ids, function(x) calc_xcs(x, ...))
 
-  do.call(rbind, understat_xCS) %>%
+  if(ungroup == T){
+    understat_xCS %>%
+      lapply(., function(x){
+        cbind(x, attacking_team = rev(x$defending_team))
+      }) %>%
+      do.call(rbind, .)
+  }
+
+    do.call(rbind, understat_xCS) %>%
     group_by(defending_team, home_away) %>%
     summarise(xCS = sum(xCS), matches = n()) %>%
     pivot_wider(names_from = home_away, values_from = c(xCS, matches)) %>%
