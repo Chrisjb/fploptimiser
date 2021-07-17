0f221b067b8cc4d08a0ee018b8daeb7d2fa6a82d
Chris Bailey <chrisbailey@Chriss-MacBook-Pro.local>
2021-07-17 16:14:05 +0100

    improved method of matching understat and fpl players

--- R/fetch_xg_data.R
+++ R/fetch_xg_data.R
@@ -18,7 +18,7 @@
 #'
 #'
 
-fetch_xg_data <- function(year = 2020, check_data=FALSE){
+fetch_xg_data <- function(year = 2021, check_data=FALSE){
 
 suppressWarnings(
   epl_teams_raw <- xml2::read_html(glue::glue('https://understat.com/league/EPL/{year}')) %>%
@@ -26,6 +26,12 @@ suppressWarnings(
     stringr::str_subset('teamsData') %>%
     stringi::stri_unescape_unicode()
 )
+
+
+  # official fpl player data/team data
+  fpl_dat <- fetch_player_data()
+
+
 
 
   team_names <- {epl_teams_raw %>%
@@ -38,6 +44,9 @@ suppressWarnings(
   teams <- data.frame(name = team_names) %>%
     arrange(name) %>%
     mutate(number=1:20)
+
+
+
 message('fetching data from understat...')
 understat <- tibble()
 
@@ -52,14 +61,25 @@ for(i in 1:nrow(teams)) {
 }
 
 
-# match understat data to fpl data by player name
-fpl_dat <- fetch_player_data()
+# team IDs may not match if comparing two different seasons so match on name instead... this list may need updating in later seasons
+team_names_common <- data.frame(fpl_name =unique(fpl_dat$name)) %>%
+  mutate(understat_name = str_replace_all(fpl_name, ' ','_'),
+         understat_name = str_replace(understat_name, 'Man_','Manchester_'),
+         understat_name = str_replace(understat_name, '_Utd','_United'),
+         understat_name =str_replace(understat_name, '(Newcastle|Sheffield)','\\1_United'),
+         understat_name =if_else(understat_name== 'Spurs','Tottenham',understat_name),
+         understat_name =if_else(understat_name== 'Wolves','Wolverhampton_Wanderers',understat_name)
+         )
 
+
+
+# match understat data to fpl data by player name
 fpl_dat2 <- fpl_dat %>%
+  left_join(team_names_common, by = c('name'= 'fpl_name')) %>%
      mutate(full_name = paste0(first_name, ' ', second_name),
             full_name = stringi::stri_trans_general(str = full_name, id = "Latin-ASCII")) %>%
-  left_join(understat, by = c('full_name' = 'player_name', 'team_id' = 'team_code')) %>%
-  select(id, full_name,  first_name, second_name, web_name, form, now_cost, team, team_id, element_type, singular_name,  minutes, goals_scored, assists, clean_sheets, goals_conceded, own_goals, penalties_saved, penalties_missed, yellow_cards, red_cards, saves, bonus, bps, influence, creativity, threat, ict_index, vapm, understat_games, understat_minutes, understat_goals, understat_assists, understat_shots, understat_key_passes, xG, xA, npg, npxG,  points_per_game, total_points)
+  left_join(understat, by = c('full_name' = 'player_name', 'understat_name' = 'team_name')) %>%
+  select(id, full_name,  first_name, second_name, web_name, form, now_cost, team= understat_name, team_id, element_type, singular_name,  minutes, goals_scored, assists, clean_sheets, goals_conceded, own_goals, penalties_saved, penalties_missed, yellow_cards, red_cards, saves, bonus, bps, influence, creativity, threat, ict_index, vapm, understat_games, understat_minutes, understat_goals, understat_assists, understat_shots, understat_key_passes, xG, xA, npg, npxG,  points_per_game, total_points)
 
 
 # match unmatched understat data to fpl data by web name
@@ -67,7 +87,7 @@ unmatched <- fpl_dat2 %>% filter(is.na(xG))
 
 found1 <- unmatched %>%
   select(-understat_games, -understat_minutes, -understat_goals, -xG, -xA, -understat_assists, -understat_shots, -understat_key_passes, -npg, -npxG) %>%
-  left_join(understat, by=c('web_name' = 'player_name', 'team_id' = 'team_code'))  %>%
+  left_join(understat, by=c('web_name' = 'player_name', 'team' = 'team_name'))  %>%
   filter(!is.na(xG))
 
 fpl_dat3 <- fpl_dat2 %>%
@@ -91,11 +111,11 @@ understat_surnames <- understat %>%
          understat_firstname = stringr::str_to_upper(understat_firstname))
 
 found2 <- fpl_dat3_unmatched %>%
-  select(-understat_games, -understat_minutes, -understat_goals, -xG, -xA, -understat_assists, -understat_shots, -understat_key_passes, -npg, -npxG, -team_name, -understat_yellow,-understat_red) %>%
+  select(-understat_games, -understat_minutes, -understat_goals, -xG, -xA, -understat_assists, -understat_shots, -understat_key_passes, -npg, -npxG, -understat_yellow,-understat_red,-team_code) %>%
   mutate(fpl_surname = unlist(lapply(stringr::str_split(fpl_dat3_unmatched$web_name, ' |-',simplify = F), function(x) last(x))),
          fpl_surname = stringi::stri_trans_general(fpl_surname,"Latin-ASCII"),
          fpl_surname = stringr::str_to_upper(fpl_surname)) %>%
-  left_join(understat_surnames, by=c('fpl_surname' = 'understat_surname', 'team_id' = 'team_code'))  %>%
+  left_join(understat_surnames, by=c('fpl_surname' = 'understat_surname', 'team' = 'team_name'))  %>%
   filter(!is.na(xG))
 
 fpl_dat4 <- fpl_dat3 %>%
@@ -109,11 +129,11 @@ fpl_dat4_unmatched <- fpl_dat4 %>%
 
 # based on FPL webname matching understat firstname
 found3 <- fpl_dat4_unmatched %>%
-  select(-understat_games, -understat_minutes, -understat_goals, -xG, -xA, -understat_assists, -understat_shots, -understat_key_passes, -npg, -npxG, -team_name, -understat_yellow,-understat_red) %>%
+  select(-understat_games, -understat_minutes, -understat_goals, -xG, -xA, -understat_assists, -understat_shots, -understat_key_passes, -npg, -npxG, -understat_yellow,-understat_red,,-team_code) %>%
   mutate(fpl_surname = unlist(lapply(stringr::str_split(fpl_dat4_unmatched$web_name, ' |-',simplify = F), function(x) last(x))),
          fpl_surname = stringi::stri_trans_general(fpl_surname,"Latin-ASCII"),
          fpl_surname = stringr::str_to_upper(fpl_surname)) %>%
-  left_join(understat_surnames, by=c('fpl_surname' = 'understat_firstname', 'team_id' = 'team_code'))  %>%
+  left_join(understat_surnames, by=c('fpl_surname' = 'understat_firstname', 'team' = 'team_name'))  %>%
   filter(!is.na(xG))
 
 fpl_dat5 <- fpl_dat4 %>%
@@ -128,9 +148,9 @@ fpl_dat5_unmatched  <- fpl_dat5 %>%
 
 # based on FPL firstname matching understat surname
 found4 <- fpl_dat5_unmatched %>%
-  select(-understat_games, -understat_minutes, -understat_goals, -xG, -xA, -understat_assists, -understat_shots, -understat_key_passes, -npg, -npxG, -team_name, -understat_yellow,-understat_red) %>%
+  select(-understat_games, -understat_minutes, -understat_goals, -xG, -xA, -understat_assists, -understat_shots, -understat_key_passes, -npg, -npxG, -understat_yellow,-understat_red,-team_code) %>%
   mutate(fpl_firstname = stringr::str_to_upper(first_name)) %>%
-  left_join(understat_surnames, by=c('fpl_firstname' = 'understat_surname', 'team_id' = 'team_code'))  %>%
+  left_join(understat_surnames, by=c('fpl_firstname' = 'understat_surname', 'team' = 'team_name'))  %>%
   filter(!is.na(xG))
 
 fpl_dat6 <- fpl_dat5 %>%
@@ -144,24 +164,78 @@ fpl_dat6_unmatched  <- fpl_dat6 %>%
 
 # based on first part of FPL surname and understat surname
 found5 <- fpl_dat6_unmatched %>%
-  select(-understat_games, -understat_minutes, -understat_goals, -xG, -xA, -understat_assists, -understat_shots, -understat_key_passes, -npg, -npxG, -team_name, -understat_yellow,-understat_red) %>%
+  select(-understat_games, -understat_minutes, -understat_goals, -xG, -xA, -understat_assists, -understat_shots, -understat_key_passes, -npg, -npxG, -understat_yellow,-understat_red, -team_code) %>%
   mutate(fpl_surname_1 = unlist(lapply(stringr::str_split(fpl_dat6_unmatched$second_name, ' |-',simplify = F), function(x) first(x))),
          fpl_surname_1 = stringr::str_to_upper(fpl_surname_1)) %>%
-  left_join(understat_surnames, by=c('fpl_surname_1' = 'understat_surname', 'team_id' = 'team_code'))  %>%
+  left_join(understat_surnames, by=c('fpl_surname_1' = 'understat_surname', 'team' = 'team_name'))  %>%
+  filter(!is.na(xG))
+
+found5_1 <- fpl_dat6_unmatched %>%
+  select(-understat_games, -understat_minutes, -understat_goals, -xG, -xA, -understat_assists, -understat_shots, -understat_key_passes, -npg, -npxG, -understat_yellow,-understat_red, -team_code) %>%
+  mutate(second_name2 = str_to_upper(second_name)) %>%
+  left_join(understat_surnames, by=c('second_name2' = 'understat_surname', 'team' = 'team_name'))  %>%
+  select(-second_name2) %>%
   filter(!is.na(xG))
 
 fpl_dat7 <- fpl_dat6 %>%
   filter(!web_name %in% found5$web_name) %>%
-  bind_rows(select(found5,-player_name,-understat_firstname,-fpl_surname_1))
+  bind_rows(select(found5,-player_name,-understat_firstname,-fpl_surname_1)) %>%
+  filter(!web_name %in% found5_1$web_name) %>%
+  bind_rows(select(found5_1,-player_name,-understat_firstname))
+
 
 # unmatched args should only be players that haven't played this season (and thus not in understat data)
 fpl_dat7_unmatched  <- fpl_dat7 %>%
   filter(is.na(xG)) %>%
   filter(minutes >0)
 
+
+found6 <- fpl_dat7_unmatched %>%
+  select(-understat_games, -understat_minutes, -understat_goals, -xG, -xA, -understat_assists, -understat_shots, -understat_key_passes, -npg, -npxG, -team_code) %>%
+  left_join(mutate(understat, player_name= str_replace_all(player_name, '-',' ')), by=c('full_name' = 'player_name', 'team' = 'team_name'))  %>%
+  filter(!is.na(xG))
+
+
+
+fpl_dat8 <- fpl_dat7 %>%
+  filter(!web_name %in% found6$web_name) %>%
+  bind_rows(found6)
+
+
+fpl_dat8_unmatched  <- fpl_dat8 %>%
+  filter(is.na(xG)) %>%
+  filter(minutes >0)
+
+
+# unmatched players may have a different club now...
+
+found7 <- fpl_dat8_unmatched %>%
+  select(-understat_games, -understat_minutes, -understat_goals, -xG, -xA, -understat_assists, -understat_shots, -understat_key_passes, -npg, -npxG, -team_code) %>%
+  left_join(understat, by=c('full_name' = 'player_name')) %>%
+  filter(!is.na(xG))
+
+fpl_dat9 <- fpl_dat8 %>%
+  filter(!web_name %in% found7$web_name) %>%
+  bind_rows(found7)
+
+
+# unmatched args should only be players that haven't played this season (and thus not in understat data)
+fpl_dat9_unmatched  <- fpl_dat9 %>%
+  filter(is.na(xG)) %>%
+  filter(minutes >0)
+
+
+
 if(check_data==TRUE){
-  message(glue::glue('{nrow(fpl_dat7_unmatched)} unmatched arguments'))
+  if(nrow(fpl_dat9_unmatched) >0){
+    message(glue::glue('{nrow(fpl_dat9_unmatched)} unmatched players: [{glue::glue_collapse(fpl_dat9_unmatched$web_name,", ")}]. Please raise an issue on Github.'))
+  } else {
+    message('0 unmatched players.')
+  }
 }
+
+
+
 
 
 # FETCH XCS DATA FROM UNDERSTAT
